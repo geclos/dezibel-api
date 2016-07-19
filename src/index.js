f9efa2263d3aed56a7e9d67a1242145354e0e4a4
geclos <clos.gerard@gmail.com>
2016-07-19 20:44:53 +0200

    added support for multiple workers with thong

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 const Hapi = require('hapi')
+const throng = require('throng')
 const routes = require('./routes')
 const middlewares = require('./middlewares')
 
-const __DEV__ = process.env.NODE_ENV === 'development'
-const server = new Hapi.Server()
+const WORKERS = process.env.WEB_CONCURRENCY || 1
 
-server.connection({
-  port: __DEV__ ? 3000 : (process.env.PORT || 3000),
-  labels: [ 'api' ]
+throng({
+  start,
+  workers: WORKERS,
+  lifetime: Infinity
 })
 
-server.register([ middlewares, routes ], err => {
-  if (err) throw err
+function start () {
+  const __DEV__ = process.env.NODE_ENV === 'development'
+  const server = new Hapi.Server()
 
-  server.start((err) => {
-    if (err) throw err
-    server.log('info', 'Server running at: ' + server.info.uri)
+  server.connection({
+    port: __DEV__ ? 3000 : (process.env.PORT || 3000),
+    labels: [ 'api' ]
   })
-})
+
+  server.register([ middlewares, routes ], err => {
+    if (err) throw err
+
+    server.start((err) => {
+      if (err) throw err
+      server.log('info', 'Server running at: ' + server.info.uri)
+    })
+  })
+}
