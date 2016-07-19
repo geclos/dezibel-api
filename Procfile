5585b7ab745b89382f1889bbdde6296d983a07c7
geclos <clos.gerard@gmail.com>
2016-07-19 09:49:12 +0200

    change heroku process name

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+api: npm start
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -13,7 +13,6 @@ const validate = function (req, decodedToken, callback) {
 }
 
 module.exports.register = (server, options, next) => {
-  console.log(process.env)
   server.app.secret = process.env.SECRET // private secret to authenticate JSON web tokens
 
   server.register(require('hapi-auth-jwt'), err => {
