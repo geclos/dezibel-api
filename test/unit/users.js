91210af7c6d6f5a9e88a147ca446d2f21de6edbb
geclos <clos.gerard@gmail.com>
2016-08-28 19:50:57 +0200

    fixed tests

--- test/unit/users.js
+++ test/unit/users.js
@@ -1,158 +1,158 @@
-const createRedisClient = require('../helpers').createRedisClient
-const users = require('../../src/core').users
-const auth = require('../../src/core').auth
 const test = require('ava')
+const Promise = require('bluebird')
+const auth = require('../../src/core').auth
+const users = require('../../src/core').users
+const createRedisClient = require('../helpers').createRedisClient
+const graph = Promise.promisifyAll(require('seraph')({
+  server: 'http://localhost:7474',
+  user: 'neo4j',
+  pass: '1234'
+}))
 
 const mockUser = {
-  id: '1',
+  uid: '1',
   name: 'Gerard',
   lastName: 'Clos',
   hash: '12345678',
   email: 'demo@dezibel.com'
 }
 
-let redis
-test.before(t => { redis = createRedisClient() })
+const db = createRedisClient().createClient()
 
-test('should create user', t => {
-  const db = redis.createClient()
+test.serial('should create user', t => {
   return createUser(db, t)
     .then(u => t.deepEqual(u, mockUser))
     .catch(err => t.fail(err.message))
 })
 
-test('should login user', t => {
-  const db = redis.createClient()
-  return createUser(db, t)
-    .then(u => {
-      const req = {
-        server: {
-          app: {
-            redis: db,
-            secret: 'password'
-          }
-        },
-        payload: { email: mockUser.email, password: mockUser.hash }
+test.serial('should login user', t => {
+  const req = {
+    server: {
+      app: {
+        redis: db,
+        graph: graph,
+        secret: 'password'
       }
+    },
+    payload: { email: mockUser.email, password: mockUser.hash }
+  }
 
-      return auth.login(req)
-        .then(user => t.deepEqual(user, mockUser))
-        .catch(err => t.fail(err.message))
-    })
+  return auth.login(req)
+    .then(user => t.deepEqual(user, mockUser))
     .catch(err => t.fail(err.message))
 })
 
-test('should login user with oauth', t => {
-  const db = redis.createClient()
-  return createUser(db, t)
-    .then(u => {
-      const req = {
-        server: {
-          app: {
-            redis: db,
-            secret: 'password'
-          }
-        },
-        auth: {
-          isAuthenticated: true,
-          credentials: {
-            profile: {
-              email: mockUser.email
-            }
-          }
+test.serial('should login user with oauth', t => {
+  const req = {
+    server: {
+      app: {
+        redis: db,
+        graph: graph,
+        secret: 'password'
+      }
+    },
+    auth: {
+      isAuthenticated: true,
+      credentials: {
+        profile: {
+          email: mockUser.email
         }
       }
+    }
+  }
 
-      return auth.loginWithOauth(req)
-        .then(user => t.deepEqual(user, mockUser))
+  return auth.loginWithOauth(req)
+    .then(user => t.deepEqual(user, mockUser))
         .catch(err => t.fail(err.message))
-    })
+})
+
+test.serial('should get user', t => {
+  const req = {
+    server: {
+      app: {
+        redis: db,
+        graph: graph,
+        secret: 'password'
+      }
+    },
+    auth: {
+      credentials: {
+        role: "ADMIN"
+      }
+    },
+    params: {id: 1}
+  }
+
+  return users.get(req)
+    .then(u => t.deepEqual(u, mockUser))
     .catch(err => t.fail(err.message))
 })
 
-test('should get user', t => {
-  const db = redis.createClient()
-  return createUser(db, t)
-    .then(u => {
-      const req = {
-        server: {
-          app: {
-            redis: db,
-            secret: 'password'
-          }
-        },
-        params: {id: 1}
+test.serial('should get all users', t => {
+  const req = {
+    server: {
+      app: {
+        redis: db,
+        graph: graph,
+        secret: 'password'
       }
+    },
+    auth: {
+      credentials: {
+        role: "ADMIN"
+      }
+    },
+    params: {},
+    query: {}
+  }
 
-      return users.get(req)
-        .then(u => t.deepEqual(u, mockUser))
-        .catch(err => t.fail(err.message))
-    })
+  return users.get(req)
+    .then(u => t.deepEqual(u, [mockUser]))
     .catch(err => t.fail(err.message))
 })
 
-test('should get all users', t => {
-  const db = redis.createClient()
-  return createUser(db, t)
-    .then(u => {
-      const req = {
-        server: {
-          app: {
-            redis: db,
-            secret: 'password'
-          }
-        },
-        params: {},
-        query: {}
+test.serial('should update user', t => {
+  const req = {
+    server: {
+      app: {
+        redis: db,
+        graph: graph,
+        secret: 'password'
       }
+    },
+    auth: {
+      credentials: {
+        role: "ADMIN"
+      }
+    },
+    params: {id: 1},
+    payload: Object.assign({}, mockUser, { name: 'changed!' })
+  }
 
-      return users.get(req)
-        .then(u => t.deepEqual(u, [mockUser]))
-        .catch(err => t.fail(err.message))
-    })
+  return users.update(req)
+    .then(u => t.deepEqual(u, req.payload))
     .catch(err => t.fail(err.message))
 })
 
-test('should update user', t => {
-  const db = redis.createClient()
-  return createUser(db, t)
-    .then(u => {
-      const req = {
-        server: {
-          app: {
-            redis: db,
-            secret: 'password'
-          }
-        },
-        params: {id: 1},
-        payload: Object.assign({}, mockUser, { name: 'changed!' })
+test.serial('should delete user', t => {
+  const req = {
+    params: {id: 1},
+    server: {
+      app: {
+        redis: db,
+        graph: graph,
+        secret: 'password'
       }
-
-      return users.update(req)
-        .then(u => t.deepEqual(u, req.payload))
-        .catch(err => t.fail(err.message))
-    })
-    .catch(err => t.fail(err.message))
-})
-
-test('should delete user', t => {
-  const db = redis.createClient()
-  return createUser(db, t)
-    .then(u => {
-      const req = {
-        params: {id: 1},
-        server: {
-          app: {
-            redis: db,
-            secret: 'password'
-          }
-        }
+    },
+    auth: {
+      credentials: {
+        role: "ADMIN"
       }
+    }
+  }
 
-      return users.delete(req)
-        .then(u => t.deepEqual(u, mockUser))
-        .catch(err => t.fail(err.message))
-    })
+  return users.delete(req)
+    .then(u => t.deepEqual(u, Object.assign({}, mockUser, {name: 'changed!'})))
     .catch(err => t.fail(err.message))
 })
 
@@ -161,6 +161,7 @@ function createUser (db, t) {
     server: {
       app: {
         redis: db,
+        graph: graph,
         secret: 'password'
       }
     },
