51932e925e41b2177d36a8ba373709844e335561
geclos <clos.gerard@gmail.com>
2016-09-01 10:38:54 +0200

    added eslint and codeclimate files

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,23 +3,25 @@ const users = require('../core').users
 exports.get = (req, reply) => {
   users.get(req)
     .then(reply)
-    .catch(err => reply(err).code(err.statusCode))
+    .catch(err => replyError(err, reply))
 }
 
 exports.create = (req, reply) => {
   users.create(req)
     .then(reply)
-    .catch(err => reply(err).code(err.statusCode))
+    .catch(err => replyError(err, reply))
 }
 
 exports.update = (req, reply) => {
   users.update(req)
     .then(reply)
-    .catch(err => reply(err).code(err.statusCode))
+    .catch(err => replyError(err, reply))
 }
 
 exports.delete = (req, reply) => {
   users.delete(req)
     .then(reply)
-    .catch(err => reply(err).code(err.statusCode))
+    .catch(err => replyError(err, reply))
 }
+
+const replyError = (err, reply) => reply(err).code(err.statusCode)
